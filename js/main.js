bf5f363dc99e8cc961eebe321cd4e1f55e068acf
김봄비 <rlawldud627@naver.com>
2021-08-07 18:48:17 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,4 @@
-// ---------- 검색(search 부분) ----------
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function(){
-  // Logic
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-  // setAttribute : searchInputEl에 그 해당 엘리먼트 속성을 지정할 때 사용
-});
-
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-  // blur : focus가 아닌 상태
-});
 
 // -------------- 뱃지 부분 --------------
 // --------------------- ScrollToPlugin --------------------(나중에 추가)
@@ -228,10 +209,6 @@ spyEls.forEach(function(spyEl){
 });
 
 
-
-// --------------- YEAR ----------------
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); //2021
 
 
 
